beb5ae5f8f0b2fda89fdad564b65daa27f13c9a1
smart-business <business.smartkenya@gmail.com>
2025-07-17 15:01:25 +0300

    done

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  esbuild: {
-    drop: mode === 'production' ? ['console', 'debugger'] : [],
-  },
 }));
