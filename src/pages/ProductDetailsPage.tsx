8e9d03e71f50b04b959ca37e93ea1eb965dd8c04
smart-business <business.smartkenya@gmail.com>
2025-07-19 14:51:38 +0300

    done

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -317,7 +317,7 @@ const ProductDetailsPage = () => {
           )}
 
           {/* Product Layout */}
-          <div className={`grid ${gridCols} gap-1 max-w-7xl`}>
+          <div className={`grid ${gridCols} gap-1`}>
             {/* Enhanced Image Gallery */}
             <div className=''>
               <EnhancedProductImageGallery product={productWithImages} />
