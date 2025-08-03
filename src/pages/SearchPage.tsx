59dee6f4c306bd8d5298867c294cd3af70f4bd24
smartkenya-business <business.smartkenya@gmail.com>
2025-08-03 18:46:53 +0300

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -8,6 +8,7 @@ import Header from '@/components/Header';
 import { ArrowLeft, ChevronLeft, SearchIcon, Settings } from 'lucide-react';
 import { isMobileUserAgent } from '@/hooks/use-mobile';
 import { Button } from '@/components/ui/button';
+import ProductSort from '@/components/products/ProductSort';
 
 const SearchPage = () => {
   const location = useLocation();
