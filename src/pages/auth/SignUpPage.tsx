3fd0e391d8de6296233eac11e7ce9a2a522571c4
smartkenya-business <business.smartkenya@gmail.com>
2025-08-05 14:17:44 +0300

    Update SignUpPage.tsx

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -149,7 +149,7 @@ const SignUpPage = () => {
   };
   
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-white flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         {/* SmartKenya Logo/Brand */}
         <div className="text-center mb-8">
