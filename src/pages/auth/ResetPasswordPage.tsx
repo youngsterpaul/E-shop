e84beddb27fb158a6243b0220ba4c91d513fb896
smartkenya-business <business.smartkenya@gmail.com>
2025-08-05 14:45:40 +0300

    Update ResetPasswordPage.tsx

--- src/pages/auth/ResetPasswordPage.tsx
+++ src/pages/auth/ResetPasswordPage.tsx
@@ -208,7 +208,7 @@ const ResetPasswordPage = () => {
   );
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center /py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow-lg rounded-lg sm:px-10">
         {/* SmartKenya Logo/Brand */}
