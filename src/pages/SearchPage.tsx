80e53add3869057d244a6ae3a85f7c0191a962f3
ephantus <220617685+smartkenya-business@users.noreply.github.com>
2025-09-01 21:00:42 +0300

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -270,7 +270,7 @@ const SearchPage = () => {
                     <span> for "<span className="font-medium text-orange-600 truncate">{searchQuery.split('(')[0].trim()}</span>"</span>
                   )}
                 </p>
-                <div className="grid grid-cols-2 sm:items-center justify-between px-2">      
+                <div className="grid grid-cols-2 items-center justify-between gap-4 px-2">      
                   <ProductSort 
                   sortOption={sortOption} 
                   onSortChange={handleSortChange}
