d4c807de32fff1d4f2f0b389e1593db49106ccbc
smart-business <220617685+smartkenya-business@users.noreply.github.com>
2025-09-03 18:03:26 +0300

    done

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -54,7 +54,7 @@ const Index = () => {
       
       <div className={`min-h-screen flex flex-col bg-gray-50`}>
         <PerformanceMonitor />
-              <main className="flex-grow mx-auto px-4 container py-8">
+        <main className={`flex-grow`}>
           <div className={`${!isMobile ? 'min-w-max' : ''}`}>
             <Header />
             <div className="relative">
