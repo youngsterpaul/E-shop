303f9396da709d44bc148ed26014f141269e3f2b
smart-business <220617685+smartkenya-business@users.noreply.github.com>
2025-09-23 18:40:31 +0100

    lovable

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    //mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
