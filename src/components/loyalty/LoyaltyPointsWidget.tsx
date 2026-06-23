cbc1413677bf0ac85e6551b4424d222985df01d6
youngsterpaul <youngsterpaul254@gmail.com>
2026-06-23 09:00:18 +0300

    12

--- src/components/loyalty/LoyaltyPointsWidget.tsx
+++ src/components/loyalty/LoyaltyPointsWidget.tsx
@@ -117,6 +117,7 @@ export const GemFashionStyleWidget = () => {
             Achievements
           </Button>
         </div>
-      </Card>
-    );
-  };
+      </CardContent> {/* Properly close CardContent first */}
+    </Card>          {/* Then close Card */}
+  );
+};
